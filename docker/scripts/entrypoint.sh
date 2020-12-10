6c559ae39c3cc1c21eec501ca86742b6b7891c87
Danielle Goncalves Prado Aguiar Martin <danigpam@br.ibm.com>
2020-12-10 14:43:02 -0300

    Update entrypoint for absolute path of java command

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Dspring.profiles.active="$APP_PROFILE" -jar /opt/simplecrud-spring/target/simplecrud-0.0.1-SNAPSHOT.jar 
+/opt/jdk-11.0.2/bin/java -Dspring.profiles.active="$APP_PROFILE" -jar /opt/simplecrud-spring/target/simplecrud-0.0.1-SNAPSHOT.jar 
